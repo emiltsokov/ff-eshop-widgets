98c8e00f6f3a2766c83b7a0ee32b1d7a98a53f36
Emil Tsokov <emil@athlonsofia.com>
2020-12-01 10:34:54 +0200

    Global ffEshopWidgets

--- src/index.js
+++ src/index.js
@@ -8,3 +8,16 @@ ReactDOM.render(
   <ProductSelection products={"777"} />,
   document.getElementById("root")
 );
+
+
+const ffEshopWidgets = (window.ffEshopWidgets = window.ffEshopWidgets || {});
+
+ffEshopWidgets.ProductSelection = (
+  element,
+  products
+) => {
+  ReactDOM.render(
+    <ProductSelection products={products} />,
+    element
+  );
+};
